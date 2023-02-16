372466156f3f9e32a567decf09f44d376765e0ba
jiamiya <jiamiya@sjtu.edu.cn>
2023-02-16 15:54:56 +0800

    fix code style

--- tactics2d/envs/parking_base.py
+++ tactics2d/envs/parking_base.py
@@ -4,10 +4,10 @@
 import sys
 sys.path.append("../")
 from math import pi, cos, sin
+from typing import List
 
 import numpy as np
 from numpy.random import randn, random
-from typing import List
 from shapely.geometry import LinearRing
 from shapely.affinity import affine_transform
 
@@ -39,14 +39,14 @@ DEBUG = True
 if DEBUG:
     import matplotlib.pyplot as plt
 
-def create_vehicle_box(x,y,heading,VehicleBox):
+def create_vehicle_box(x,y,heading,vehicle_box):
     cos_theta = np.cos(heading)
     sin_theta = np.sin(heading)
     mat = [cos_theta, -sin_theta, sin_theta, cos_theta, x, y]
-    return affine_transform(VehicleBox, mat)
+    return affine_transform(vehicle_box, mat)
 
-def get_box_coords(x,y,heading,VehicleBox):
-    vehicle_box = create_vehicle_box(x,y,heading,VehicleBox)
+def get_box_coords(x,y,heading,vehicle_box):
+    vehicle_box = create_vehicle_box(x,y,heading,vehicle_box)
     return list(vehicle_box.coords)[:-1]
 
 def random_gaussian_num(mean, std, clip_low, clip_high):
@@ -64,7 +64,7 @@ def get_rand_pos(origin_x, origin_y, angle_min, angle_max, radius_min, radius_ma
     radius_rand = random_gaussian_num((radius_min+radius_max)/2, (radius_max-radius_min)/4, radius_min, radius_max)
     return origin_x+cos(angle_rand)*radius_rand, origin_y+sin(angle_rand)*radius_rand
 
-def generate_bay_parking(VehicleBox):
+def generate_bay_parking(vehicle_box):
     '''
     Generate the parameters that a bay parking case need.
     Returns
@@ -90,11 +90,11 @@ def generate_bay_parking(VehicleBox):
 
     # generate dest
     dest_yaw = random_gaussian_num(pi/2, pi/36, pi*5/12, pi*7/12)
-    rb, _, _, lb  = get_box_coords(origin[0], origin[1], dest_yaw, VehicleBox)
+    rb, _, _, lb  = get_box_coords(origin[0], origin[1], dest_yaw, vehicle_box)
     min_dest_y = -min(rb[1], lb[1]) + MIN_DIST_TO_OBST
     dest_x = origin[0]
     dest_y = random_gaussian_num(min_dest_y+0.4, 0.2, min_dest_y, min_dest_y+0.8)
-    car_rb, car_rf, car_lf, car_lb  = get_box_coords(dest_x, dest_y, dest_yaw, VehicleBox)
+    car_rb, car_rf, car_lf, car_lb  = get_box_coords(dest_x, dest_y, dest_yaw, vehicle_box)
     dest_box = LinearRing((car_rb, car_rf, car_lf, car_lb))
     if DEBUG:
         ax.add_patch(plt.Polygon(xy=list([car_rb, car_rf, car_lf, car_lb]), color='r'))
@@ -116,10 +116,10 @@ def generate_bay_parking(VehicleBox):
         min_dist_to_obst = 0.4 + MIN_DIST_TO_OBST
         left_car_x = origin[0] - (WIDTH + random_uniform_num(min_dist_to_obst, max_dist_to_obst))
         left_car_yaw = random_gaussian_num(pi/2, pi/36, pi*5/12, pi*7/12)
-        rb, _, _, lb  = get_box_coords(left_car_x, origin[1], left_car_yaw, VehicleBox)
+        rb, _, _, lb  = get_box_coords(left_car_x, origin[1], left_car_yaw, vehicle_box)
         min_left_car_y = -min(rb[1], lb[1]) + MIN_DIST_TO_OBST
         left_car_y = random_gaussian_num(min_left_car_y+0.4, 0.2, min_left_car_y, min_left_car_y+0.8)
-        obstacle_left = create_vehicle_box( left_car_x, left_car_y, left_car_yaw, VehicleBox)
+        obstacle_left = create_vehicle_box( left_car_x, left_car_y, left_car_yaw, vehicle_box)
 
     # generate obstacle on right
     dist_dest_to_left_obst = dest_box.distance(obstacle_left)
@@ -136,10 +136,10 @@ def generate_bay_parking(VehicleBox):
     else: # generate another vehicle as obstacle on right
         right_car_x = origin[0] + (WIDTH + random_uniform_num(min_dist_to_obst, max_dist_to_obst))
         right_car_yaw = random_gaussian_num(pi/2, pi/36, pi*5/12, pi*7/12)
-        rb, _, _, lb  = get_box_coords(right_car_x, origin[1], right_car_yaw, VehicleBox)
+        rb, _, _, lb  = get_box_coords(right_car_x, origin[1], right_car_yaw, vehicle_box)
         min_right_car_y = -min(rb[1], lb[1]) + MIN_DIST_TO_OBST
         right_car_y = random_gaussian_num(min_right_car_y+0.4, 0.2, min_right_car_y, min_right_car_y+0.8)
-        obstacle_right = create_vehicle_box(right_car_x, right_car_y, right_car_yaw, VehicleBox)
+        obstacle_right = create_vehicle_box(right_car_x, right_car_y, right_car_yaw, vehicle_box)
     # print(dist_dest_to_left_obst, dest_box.distance(obstacle_right), dest_box.distance(obstacle_right)+dist_dest_to_left_obst)
     dist_dest_to_right_obst = dest_box.distance(obstacle_right)
     if dist_dest_to_right_obst+dist_dest_to_left_obst<MIN_BAY_PARK_LOT_WIDTH-WIDTH or \
@@ -174,7 +174,7 @@ def generate_bay_parking(VehicleBox):
             obs_x = random_uniform_num(*valid_obst_x_range)
             obs_y = random_uniform_num(*valid_obst_y_range)
             obs_yaw = random()*pi*2
-            obs_coords = np.array(get_box_coords(obs_x, obs_y, obs_yaw, VehicleBox))
+            obs_coords = np.array(get_box_coords(obs_x, obs_y, obs_yaw, vehicle_box))
             obs = LinearRing(obs_coords+0.5*random(obs_coords.shape))
             if obs.intersects(other_obstacle_range):
                 continue
@@ -182,12 +182,10 @@ def generate_bay_parking(VehicleBox):
 
     # merge two kind of obstacles
     obstacles.extend(other_obstcales)
-
     if DEBUG:
         for obs in obstacles:
             ax.add_patch(plt.Polygon(xy=list(obs.coords), color='b'))
 
-    
     # generate start position
     start_box_valid = False
     valid_start_x_range = (origin[0]-bay_half_len/2, origin[0]+bay_half_len/2)
@@ -198,7 +196,7 @@ def generate_bay_parking(VehicleBox):
         start_y = random_uniform_num(*valid_start_y_range)
         start_yaw = random_gaussian_num(0, pi/6, -pi/2, pi/2)
         start_yaw = start_yaw+pi if random()<0.5 else start_yaw
-        start_box = create_vehicle_box(start_x, start_y, start_yaw, VehicleBox)
+        start_box = create_vehicle_box(start_x, start_y, start_yaw, vehicle_box)
         # check collision
         for obst in obstacles:
             if obst.intersects(start_box):
@@ -215,19 +213,19 @@ def generate_bay_parking(VehicleBox):
     for obs in obstacles:
         if random()<0.1:
             obstacles.remove(obs)
-    
     if DEBUG:
-        ax.add_patch(plt.Polygon(xy=get_box_coords(start_x, start_y, start_yaw, VehicleBox), color='g'))
+        ax.add_patch(plt.Polygon(xy=get_box_coords(start_x, start_y, start_yaw, vehicle_box), color='g'))
         print(generate_success)
         plt.show()
     
+    # return if generate successfully
     if generate_success:
         return [start_x, start_y, start_yaw], [dest_x, dest_y, dest_yaw], obstacles
     else:
         # print(1)
-        return generate_bay_parking(VehicleBox)
+        return generate_bay_parking(vehicle_box)
 
-def generate_parallel_parking(VehicleBox):
+def generate_parallel_parking(vehicle_box):
     '''
     Generate the parameters that a parallel parking case need.
     
@@ -255,11 +253,11 @@ def generate_parallel_parking(VehicleBox):
 
     # generate dest
     dest_yaw = random_gaussian_num(0, pi/36, -pi/12, pi/12)
-    rb, rf, _, _  = get_box_coords(origin[0], origin[1], dest_yaw, VehicleBox)
+    rb, rf, _, _  = get_box_coords(origin[0], origin[1], dest_yaw, vehicle_box)
     min_dest_y = -min(rb[1], rf[1]) + MIN_DIST_TO_OBST
     dest_x = origin[0]
     dest_y = random_gaussian_num(min_dest_y+0.4, 0.2, min_dest_y, min_dest_y+0.8)
-    car_rb, car_rf, car_lf, car_lb  = get_box_coords(dest_x, dest_y, dest_yaw, VehicleBox)
+    car_rb, car_rf, car_lf, car_lb  = get_box_coords(dest_x, dest_y, dest_yaw, vehicle_box)
     dest_box = LinearRing((car_rb, car_rf, car_lf, car_lb))
 
     if DEBUG:
@@ -282,10 +280,10 @@ def generate_parallel_parking(VehicleBox):
         min_dist_to_obst = 0.4 + MIN_DIST_TO_OBST
         left_car_x = origin[0] - (LENGTH + random_uniform_num(min_dist_to_obst, max_dist_to_obst))
         left_car_yaw = random_gaussian_num(0, pi/36, -pi/12, pi/12)
-        rb, rf, _, _  = get_box_coords(left_car_x, origin[1], left_car_yaw, VehicleBox)
+        rb, rf, _, _  = get_box_coords(left_car_x, origin[1], left_car_yaw, vehicle_box)
         min_left_car_y = -min(rb[1], rf[1]) + MIN_DIST_TO_OBST
         left_car_y = random_gaussian_num(min_left_car_y+0.4, 0.2, min_left_car_y, min_left_car_y+0.8)
-        obstacle_left = create_vehicle_box(left_car_x, left_car_y, left_car_yaw, VehicleBox)
+        obstacle_left = create_vehicle_box(left_car_x, left_car_y, left_car_yaw, vehicle_box)
 
     # generate obstacle on right
     dist_dest_to_left_obst = dest_box.distance(obstacle_left)
@@ -302,10 +300,10 @@ def generate_parallel_parking(VehicleBox):
     else: # generate another vehicle as obstacle on right
         right_car_x = origin[0] + (LENGTH + random_uniform_num(min_dist_to_obst, max_dist_to_obst))
         right_car_yaw = random_gaussian_num(0, pi/36, -pi/12, pi/12)
-        rb, rf, _, _  = get_box_coords(right_car_x, origin[1], right_car_yaw, VehicleBox)
+        rb, rf, _, _  = get_box_coords(right_car_x, origin[1], right_car_yaw, vehicle_box)
         min_right_car_y = -min(rb[1], rf[1]) + MIN_DIST_TO_OBST
         right_car_y = random_gaussian_num(min_right_car_y+0.4, 0.2, min_right_car_y, min_right_car_y+0.8)
-        obstacle_right = create_vehicle_box(right_car_x, right_car_y, right_car_yaw, VehicleBox)
+        obstacle_right = create_vehicle_box(right_car_x, right_car_y, right_car_yaw, vehicle_box)
     # print(dist_dest_to_left_obst, dest_box.distance(obstacle_right), dest_box.distance(obstacle_right)+dist_dest_to_left_obst)
     dist_dest_to_right_obst = dest_box.distance(obstacle_right)
     if dist_dest_to_right_obst+dist_dest_to_left_obst<LENGTH/4 or \
@@ -341,7 +339,7 @@ def generate_parallel_parking(VehicleBox):
             obs_x = random_uniform_num(*valid_obst_x_range)
             obs_y = random_uniform_num(*valid_obst_y_range)
             obs_yaw = random()*pi*2
-            obs_coords = np.array(get_box_coords(obs_x, obs_y, obs_yaw, VehicleBox))
+            obs_coords = np.array(get_box_coords(obs_x, obs_y, obs_yaw, vehicle_box))
             obs = LinearRing(obs_coords+0.5*random(obs_coords.shape))
             if obs.intersects(other_obstacle_range):
                 continue
@@ -349,11 +347,9 @@ def generate_parallel_parking(VehicleBox):
 
     # merge two kind of obstacles
     obstacles.extend(other_obstcales)
-
     if DEBUG:
         for obs in obstacles:
             ax.add_patch(plt.Polygon(xy=list(obs.coords), color='b'))
-
     
     # generate start position
     start_box_valid = False
@@ -365,7 +361,7 @@ def generate_parallel_parking(VehicleBox):
         start_y = random_uniform_num(*valid_start_y_range)
         start_yaw = random_gaussian_num(0, pi/6, -pi/2, pi/2)
         start_yaw = start_yaw+pi if random()<0.5 else start_yaw
-        start_box = create_vehicle_box(start_x, start_y, start_yaw, VehicleBox)
+        start_box = create_vehicle_box(start_x, start_y, start_yaw, vehicle_box)
         # check collision
         for obst in obstacles:
             if obst.intersects(start_box):
@@ -378,7 +374,6 @@ def generate_parallel_parking(VehicleBox):
                 print('start box intersect with dest box, will retry to generate.')
             start_box_valid = False
     
-    
     # flip the dest box so that the orientation of start matches the dest
     if cos(start_yaw)<0:
         dest_box_center = np.mean(np.array(dest_box.coords[:-1]), axis=0)
@@ -390,16 +385,16 @@ def generate_parallel_parking(VehicleBox):
     for obs in obstacles:
         if random()<0.1:
             obstacles.remove(obs)
-    
     if DEBUG:
-        ax.add_patch(plt.Polygon(xy=get_box_coords(start_x, start_y, start_yaw, VehicleBox), color='g'))
+        ax.add_patch(plt.Polygon(xy=get_box_coords(start_x, start_y, start_yaw, vehicle_box), color='g'))
         print(generate_success)
         plt.show()
     
+    # return if success
     if generate_success:
         return [start_x, start_y, start_yaw], [dest_x, dest_y, dest_yaw], obstacles
     else:
-        return generate_parallel_parking(VehicleBox)
+        return generate_parallel_parking(vehicle_box)
 
 # TODO use class `Area` from tactics2d.map_base.area
 class Area(object):
@@ -416,7 +411,7 @@ class Area(object):
 
 class ParkingMapNormal(Map):
     def __init__(self,name="CarParking", scenario_type="parking"):
-        super.__init__(name, scenario_type)
+        super().__init__(name, scenario_type)
 
         self.start: Position = None
         self.dest: Position = None
