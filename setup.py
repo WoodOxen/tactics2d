eabcf6b439237204fcd73ccd1d287719df617d61
MotacillaAlba <rowena.lily.lee@gmail.com>
2023-09-01 00:00:41 +0800

    👷 : working on pypi publish workflow

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-REQUIRED = ["pyproj", "shapely", "gym"]
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name="tactics2d",
-    version="0.0.1",
-    author="Yueyuan Li",
-    author_email="rowena.academic@gmail.com",
-    description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=find_packages(),
-    install_requires=REQUIRED,
-    python_requires=">=3.7.0",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: POSIX :: Linux",
-    ],
-)
+setup()
