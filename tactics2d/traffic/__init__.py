b13eda7a0d2ff1f6fe85d2c7e410794312500bdc
MotacillaAlba <rowena.lily.lee@gmail.com>
2024-02-24 21:57:49 +0800

    🔨 update dependency for CI

--- tactics2d/traffic/__init__.py
+++ tactics2d/traffic/__init__.py
@@ -1,3 +1,11 @@
-from .scenario_manager import ScenarioManager
+##! python3
+# Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
+# @File: __init__.py
+# @Description: Initialize the traffic module.
+# @Author: Yueyuan Li
+# @Version: 1.0.0
 
-__all__ = ["ScenarioManager"]
+from .scenario_manager import ScenarioManager
+from .status import ScenarioStatus, TrafficStatus
+
+__all__ = ["ScenarioManager", "ScenarioStatus", "TrafficStatus"]
