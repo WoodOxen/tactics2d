ce7a0b445a1ed4b2dd32145f5c74f3e485848efe
182210301114@stu.just.edu.cn <182210301114@stu.just.edu.cn>
2024-08-21 23:25:46 +0800

    Add carla sensor base class

--- tactics2d/sensor/__init__.py
+++ tactics2d/sensor/__init__.py
@@ -9,5 +9,6 @@ from .camera import TopDownCamera
 from .lidar import SingleLineLidar
 from .render_manager import RenderManager
 from .sensor_base import SensorBase
+from .carla_sensor_base import CarlaSensorBase
 
-__all__ = ["SensorBase", "TopDownCamera", "SingleLineLidar", "RenderManager"]
+__all__ = ["SensorBase", "TopDownCamera", "SingleLineLidar", "RenderManager", "CarlaSensorBase"]
