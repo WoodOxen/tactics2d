6efc0f92a5d0a66858e03117510b58b4a7c0d3af
MotacillaAlba <rowena.lily.lee@gmail.com>
2025-05-14 00:51:11 +0800

    📝 `hide input` please work

--- docs/javascripts/hide_code.js
+++ docs/javascripts/hide_code.js
@@ -1,29 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var cells = document.querySelectorAll('div.jp-Cell');
+  var cells = document.querySelectorAll('.celltag_hide_input');
 
   cells.forEach(function (cell) {
-    if (cell.innerHTML.includes('tag_hide_input')) {
-      var input = cell.querySelector('.jp-InputArea');
+    var input = cell.querySelector('.jp-InputArea');
 
-      if (input) {
-        input.style.display = 'none';
+    if (input) {
+      input.style.display = 'none';
 
-        var button = document.createElement('button');
-        button.innerText = 'Show Code';
-        button.classList.add('show-code-button');
+      var button = document.createElement('button');
+      button.innerText = 'Show Code';
+      button.classList.add('show-code-button');
 
-        button.onclick = function () {
-          if (input.style.display === 'none') {
-            input.style.display = '';
-            button.innerText = 'Hide Code';
-          } else {
-            input.style.display = 'none';
-            button.innerText = 'Show Code';
-          }
-        };
+      button.onclick = function () {
+        if (input.style.display === 'none') {
+          input.style.display = '';
+          button.innerText = 'Hide Code';
+        } else {
+          input.style.display = 'none';
+          button.innerText = 'Show Code';
+        }
+      };
 
-        cell.insertBefore(button, input);
-      }
+      cell.insertBefore(button, input);
     }
   });
 });
