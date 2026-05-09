a37a6bcd29ead4eb8d5ef8608cd7aebc81551bf0
bitxige <3191264548@qq.com>
2026-05-09 10:53:12 +0800

    fix: reorder proto imports so dataset_pb2 loads before compressed_lidar_pb2

--- tactics2d/dataset_parser/womd_proto/pb3/__init__.py
+++ tactics2d/dataset_parser/womd_proto/pb3/__init__.py
@@ -1,11 +1,12 @@
+# isort: skip_file
 # Copyright (C) 2024, Tactics2D Authors. Released under the GNU GPLv3.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """pb3 module - Protobuf generated files for protobuf >= 3.21.0."""
 
 from . import (
-    compressed_lidar_pb2,
     dataset_pb2,
+    compressed_lidar_pb2,
     keypoint_pb2,
     label_pb2,
     map_pb2,
