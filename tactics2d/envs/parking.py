1bdf10110b33616de592e48362212c27dd206e30
VOY <voyager_kowalski@outlook.com>
2026-05-30 22:26:40 +0800

    Restore pygame renderer under renderer module

--- tactics2d/envs/parking.py
+++ tactics2d/envs/parking.py
@@ -17,7 +17,7 @@ from tactics2d.map.element import Map
 from tactics2d.map.generator import ParkingLotGenerator
 from tactics2d.participant.element import Vehicle
 from tactics2d.physics import SingleTrackKinematics
-from tactics2d.sensor import BEVCamera, SingleLineLidar
+from tactics2d.renderer import RenderManager, SingleLineLidar, TopDownCamera
 from tactics2d.traffic import ScenarioManager, ScenarioStatus, TrafficStatus
 from tactics2d.traffic.event_detection import (
     Arrival,
@@ -336,9 +336,9 @@ class ParkingEnv(gym.Env):
             self.target_heading = None
             self.cnt_step = 0
 
-            # self.render_manager = RenderManager(
-            #     fps=self.render_fps, windows_size=self._window_size, off_screen=self.off_screen
-            # )
+            self.render_manager = RenderManager(
+                fps=self.render_fps, windows_size=self._window_size, off_screen=self.off_screen
+            )
 
             # traffic event detectors
             self.status_checklist = {
@@ -409,7 +409,7 @@ class ParkingEnv(gym.Env):
             # reset the sensors
             self.render_manager.reset()
 
-            camera = BEVCamera(
+            camera = TopDownCamera(
                 id_=0,
                 map_=self.map_,
                 perception_range=(20, 20, 20, 20),
